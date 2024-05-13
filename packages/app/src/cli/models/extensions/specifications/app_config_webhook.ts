17f69e1315ea32e234c3b3b74ae685f007e9cceb
Isaac Roldán <isaac.roldan@shopify.com>
2024-05-13 17:39:42 +0200

    Move mergeAllWebhooks logic to the transform files

--- packages/app/src/cli/models/extensions/specifications/app_config_webhook.ts
+++ packages/app/src/cli/models/extensions/specifications/app_config_webhook.ts
@@ -1,6 +1,5 @@
 import {WebhooksSchema} from './app_config_webhook_schemas/webhooks_schema.js'
 import {transformToWebhookConfig, transformFromWebhookConfig} from './transform/app_config_webhook.js'
-import {WebhookSubscription} from './types/app_config_webhook.js'
 import {CustomTransformationConfig, createConfigExtensionSpecification} from '../specification.js'
 
 export const WebhooksSpecIdentifier = 'webhooks'
@@ -17,28 +16,3 @@ const appWebhooksSpec = createConfigExtensionSpecification({
 })
 
 export default appWebhooksSpec
-
-export function mergeAllWebhooks(subscriptions: WebhookSubscription[]): WebhookSubscription[] {
-  return subscriptions.reduce((accumulator, subscription) => {
-    const existingSubscription = accumulator.find(
-      (sub) =>
-        sub.uri === subscription.uri &&
-        sub.sub_topic === subscription.sub_topic &&
-        sub.include_fields === subscription.include_fields &&
-        sub.filter === subscription.filter,
-    )
-    if (existingSubscription) {
-      if (subscription.compliance_topics) {
-        existingSubscription.compliance_topics ??= []
-        existingSubscription.compliance_topics.push(...subscription.compliance_topics)
-      }
-      if (subscription.topics) {
-        existingSubscription.topics ??= []
-        existingSubscription.topics.push(...subscription.topics)
-      }
-    } else {
-      accumulator.push(subscription)
-    }
-    return accumulator
-  }, [] as WebhookSubscription[])
-}
