95407dd0ed2712f8915e79869397a14543f91d43
James Meng <james.meng@shopify.com>
2024-05-14 17:36:22 -0700

    Extract Theme Reconciliation logic to its own file

--- packages/theme/src/cli/utilities/asset-file-syncer.test.ts
+++ packages/theme/src/cli/utilities/asset-file-syncer.test.ts
@@ -1,115 +1,38 @@
-import {LOCAL_STRATEGY, REMOTE_STRATEGY, initializeThemeEditorSync} from './asset-file-syncer.js'
 import {fakeThemeFileSystem} from './theme-fs/theme-fs-mock-factory.js'
-import {deleteThemeAsset, fetchThemeAsset} from '@shopify/cli-kit/node/themes/api'
+import {reconcileThemeFiles} from './theme-environment/theme-reconciliation.js'
+import {initializeThemeEditorSync} from './asset-file-syncer.js'
+import {pollThemeEditorChanges} from './theme-environment/theme-polling.js'
+import {fetchChecksums} from '@shopify/cli-kit/node/themes/api'
 import {buildTheme} from '@shopify/cli-kit/node/themes/factories'
-import {Checksum, ThemeAsset} from '@shopify/cli-kit/node/themes/types'
+import {ThemeAsset} from '@shopify/cli-kit/node/themes/types'
 import {DEVELOPMENT_THEME_ROLE} from '@shopify/cli-kit/node/themes/utils'
-import {renderSelectPrompt} from '@shopify/cli-kit/node/ui'
 import {describe, expect, test, vi} from 'vitest'
 
-vi.mock('@shopify/cli-kit/node/ui')
 vi.mock('@shopify/cli-kit/node/themes/api')
-vi.mock('./theme-fs.js')
-vi.mock('./theme-downloader.js')
-vi.mock('./theme-uploader.js')
+vi.mock('./theme-environment/theme-reconciliation.js')
+vi.mock('./theme-environment/theme-polling.js')
 
-describe('initializeThemeEditorSync', () => {
+describe('initializeThemeEditorSync', async () => {
   const developmentTheme = buildTheme({id: 1, name: 'Theme', role: DEVELOPMENT_THEME_ROLE})!
   const adminSession = {token: '', storeFqdn: ''}
-  const remoteChecksums: Checksum[] = []
   const files = new Map<string, ThemeAsset>([])
   const defaultThemeFileSystem = fakeThemeFileSystem('tmp', files)
 
-  describe('files only present on remote developer theme', () => {
-    test('should download assets from remote theme when `remote` source is selected', async () => {
-      // Given
-      vi.mocked(renderSelectPrompt).mockResolvedValue(REMOTE_STRATEGY)
-      const assetToBeDownloaded = {checksum: '2', key: 'templates/second_asset.json', value: 'content'}
-      const remoteChecksums = [assetToBeDownloaded]
+  test('should call pollThemeEditorChanges with updated checksums if the remote theme was been updated during reconciliation', async () => {
+    // Given
+    const initialRemoteChecksums = [{checksum: '1', key: 'templates/asset.json'}]
+    vi.mocked(reconcileThemeFiles).mockResolvedValue(undefined)
+    vi.mocked(fetchChecksums).mockResolvedValue([{checksum: '2', key: 'templates/asset.json'}])
 
-      vi.mocked(fetchThemeAsset).mockResolvedValue(assetToBeDownloaded)
+    // When
+    await initializeThemeEditorSync(developmentTheme, adminSession, initialRemoteChecksums, defaultThemeFileSystem)
 
-      // When
-      expect(defaultThemeFileSystem.files.get('templates/asset.json')).toBeUndefined()
-      await initializeThemeEditorSync(developmentTheme, adminSession, remoteChecksums, defaultThemeFileSystem)
-
-      // Then
-      expect(fetchThemeAsset).toHaveBeenCalledWith(developmentTheme.id, assetToBeDownloaded.key, adminSession)
-      expect(defaultThemeFileSystem.files.get('templates/second_asset.json')).toEqual(assetToBeDownloaded)
-    })
-
-    test('should delete assets from local disk when `local` source is selected', async () => {
-      // Given
-      vi.mocked(renderSelectPrompt).mockResolvedValue(LOCAL_STRATEGY)
-      const assetToBeDeleted = {checksum: '2', key: 'templates/asset.json'}
-      const remoteChecksums = [assetToBeDeleted]
-
-      // When
-      await initializeThemeEditorSync(developmentTheme, adminSession, remoteChecksums, defaultThemeFileSystem)
-
-      // Then
-      expect(deleteThemeAsset).toHaveBeenCalledWith(developmentTheme.id, assetToBeDeleted.key, adminSession)
-      expect(defaultThemeFileSystem.files.get('templates/asset.json')).toBeUndefined()
-    })
-  })
-
-  describe('files only present on local developer theme', () => {
-    test('should delete files from local disk when `remote` source is selected', async () => {
-      // Given
-      vi.mocked(renderSelectPrompt).mockResolvedValue(REMOTE_STRATEGY)
-      const files = new Map([['templates/asset.json', {checksum: '1', key: 'templates/asset.json'}]])
-      const localThemeFileSystem = fakeThemeFileSystem('tmp', files)
-      const spy = vi.spyOn(localThemeFileSystem, 'delete')
-
-      // When
-      await initializeThemeEditorSync(developmentTheme, adminSession, remoteChecksums, localThemeFileSystem)
-
-      // Then
-      expect(spy).toHaveBeenCalledWith('templates/asset.json')
-    })
-
-    test('should not delete files from local disk when `local` source is selected', async () => {
-      // Given
-      vi.mocked(renderSelectPrompt).mockResolvedValue(LOCAL_STRATEGY)
-      const files = new Map([['templates/asset.json', {checksum: '1', key: 'templates/asset.json'}]])
-      const localThemeFileSystem = fakeThemeFileSystem('tmp', files)
-      const spy = vi.spyOn(localThemeFileSystem, 'delete')
-
-      // When
-      await initializeThemeEditorSync(developmentTheme, adminSession, remoteChecksums, localThemeFileSystem)
-
-      // Then
-      expect(spy).not.toHaveBeenCalled()
-    })
-  })
-
-  describe('files with conflicting checksums', () => {
-    test('should download files from remote theme when `remote` source is selected', async () => {
-      // Given
-      vi.mocked(renderSelectPrompt).mockResolvedValue(REMOTE_STRATEGY)
-      const files = new Map([['templates/asset.json', {checksum: '1', key: 'templates/asset.json'}]])
-      const localThemeFileSystem = fakeThemeFileSystem('tmp', files)
-      const remoteChecksums = [{checksum: '2', key: 'templates/asset.json'}]
-
-      // When
-      await initializeThemeEditorSync(developmentTheme, adminSession, remoteChecksums, localThemeFileSystem)
-
-      // Then
-      expect(fetchThemeAsset).toHaveBeenCalled()
-    })
-
-    test('should not download files from remote when `local` source is selected', async () => {
-      // Given
-      vi.mocked(renderSelectPrompt).mockResolvedValue(LOCAL_STRATEGY)
-      const files = new Map([['templates/asset.json', {checksum: '1', key: 'templates/asset.json'}]])
-      const localThemeFileSystem = fakeThemeFileSystem('tmp', files)
-      const remoteChecksums = [{checksum: '2', key: 'templates/asset.json'}]
-
-      // When
-      await initializeThemeEditorSync(developmentTheme, adminSession, remoteChecksums, localThemeFileSystem)
-
-      // Then
-      expect(fetchThemeAsset).not.toHaveBeenCalled()
-    })
+    // Then
+    expect(pollThemeEditorChanges).toHaveBeenCalledWith(
+      developmentTheme,
+      adminSession,
+      [{checksum: '2', key: 'templates/asset.json'}],
+      defaultThemeFileSystem,
+    )
   })
 })
